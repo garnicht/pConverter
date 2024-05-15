217da9113a20758eeddb08f3eba37a31b8599e07
Leon Hocker <leonhocker@Air-von-Leon.fritz.box>
2024-05-15 11:34:29 +0200

    changed install_requirements.py to execute different code dependent on operating system

--- install_requirements.py
+++ install_requirements.py
@@ -1,3 +1,12 @@
 import subprocess
+import platform
 
-subprocess.check_call(["python3", "-m", "pip", "install", "-r", "requirements.txt"])
+if platform.system() == "Darwin":
+    subprocess.check_call(["python3", "-m", "pip", "install", "-r", "requirements.txt"])
+
+elif platform.system() == "Windows":
+    subprocess.check_call(["python", "-m", "pip", "install", "-r", "requirements.txt"])
+
+else: 
+    system = platform.system()
+    print(f"This operating system {system} is not supported yet")

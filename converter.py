3dd534fe0542b973ab82018a144827d190c8b331
Leon Hocker <leonhocker@Air-von-Leon.fritz.box>
2024-07-01 13:09:45 +0200

    changed the code so script is usable for new csv design getting from pcon. Change not uses headlines to 'platzhalter' name. Looks like weclapp NEEDS the headlin names...gosh

--- converter.py
+++ converter.py
@@ -1,54 +1,86 @@
-# %%
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import pandas as pd
 import numpy as np
-import os 
 
-# %%
+
+# In[2]:
+
+
 # transform csv to df
 try:
     artikelliste_df = pd.read_csv("import.csv", sep=";")
 except Exception as e:
     print(e)
-artikelliste_df.head()
 
-# %%
+
+# In[3]:
+
+
+# get rid of empty Strings
+artikelliste_df = artikelliste_df.apply(lambda x: x.replace("", None))
+
+
+# In[4]:
+
+
+artikelliste_df = artikelliste_df.query("Pos.notnull()")
+
+
+# In[5]:
+
+
 # gourp by Bezeichnung aka Artikelnummer to cumulate dublicates and get rid of NaN rows
 artikelliste_df = artikelliste_df.groupby("Bezeichnung")["Menge"].sum().reset_index()
 
-# %%
+
+# In[6]:
+
+
 # handle the columns we need / don't need
 try: 
     artikelliste_df.insert(0,"Kopfartikelnummer",np.nan)
-    artikelliste_df.insert(1,"Parent article name",np.nan)
-    artikelliste_df.insert(2,"Parent article description",np.nan)
-    artikelliste_df.insert(4,"Article name",np.nan)
-    artikelliste_df.insert(5,"Article description",np.nan)
-    artikelliste_df.insert(7,"Position no.",np.nan)
-except Exception as e:
-    print(e) 
-       
-artikelliste_df
-
-# %%
-#change name of existing columns
-try:
-    artikelliste_df.rename(columns={"Bezeichnung":"Artikelnummer","Menge":"QUANTITY"},inplace=True)
+    artikelliste_df.insert(1,"platzhalter1",np.nan)
+    artikelliste_df.insert(2,"platzhalter2",np.nan)
+    artikelliste_df.insert(3,"platzhalter3",np.nan)
+    artikelliste_df.insert(4,"platzhalter4",np.nan)
+    artikelliste_df.insert(5,"platzhalter5",np.nan)
 except Exception as e:
     print(e)
-    
-artikelliste_df.head()
 
-# %%
+
+# In[7]:
+
+
+#change name of existing columns
+try:
+    artikelliste_df.rename(columns={"Bezeichnung":"Artikelnummer","Menge":"Anzahl"},inplace=True)
+except Exception as e:
+    print(e)
+
+
+# In[8]:
+
+
 # ask for input and insert into df
 artikelnummer = input("Wie lautet die Kopfartikelnummer?:")
 artikelliste_df["Kopfartikelnummer"] = artikelnummer
-artikelliste_df.head(10)
 
-# %%
-artikelnummer_dublicates_df = artikelliste_df[artikelliste_df["Artikelnummer"].duplicated(keep=False)]
-artikelnummer_dublicates_df.head()
 
-# %%
+# In[9]:
+
+
+# get rid of dublicates
+if not artikelliste_df[artikelliste_df["Artikelnummer"].duplicated()].empty:
+    artikelliste_df = artikelliste_df[artikelliste_df["Artikelnummer"].duplicated(keep=False)]
+
+
+# In[10]:
+
+
 artikelliste_df.to_csv(f"finished_{artikelnummer}.csv", index=False, sep=";")
-
 

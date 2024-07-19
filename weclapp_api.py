643f63e5a39c87fc9d9a00739fb7b5a4a9a08824
Leon Hocker <leonhocker@MacBook-Air-von-Leon.fritz.box>
2024-07-19 13:39:51 +0200

    updated create_article function, it now automatically creates an article with the correct check boxes and options.

--- weclapp_api.py
+++ weclapp_api.py
@@ -17,20 +17,18 @@ load_dotenv()
 
 # In[ ]:
 
-
 def create_article(article_number, article_name):
     url = "https://degridstudio.weclapp.com/webapp/api/v1/article"
     token = os.getenv("weclapp_token")
     headers = {"AuthenticationToken" : token,
                "Content-Type" : "application/json"}
-    content = {"articleNumber" : article_number,
-               "name" : article_name,
-               "articleCategoryId": "48583",
-               'articleType': 'SALES_BILL_OF_MATERIAL',
-               'availableForSalesChannels': [],
-               'availableInSale': True,
-               'batchNumberRequired': False,
-               'billOfMaterialPartDeliveryPossible': True,}
+    content = {"articleNumber" : article_number,                    # required
+               "name" : article_name,                               # required
+               "articleCategoryId": "48583",                        # ID-48583 steht für Konfigurationen
+               'articleType': 'SALES_BILL_OF_MATERIAL',             # Verkaufstückliste
+               'billOfMaterialPartDeliveryPossible': True,          # Stücklistenteillieferung möglich
+               'useSalesBillOfMaterialItemPrices': False,           # Unterpositionen mit Preisen für Verkauf
+               'useSalesBillOfMaterialItemPricesForPurchase': True} # Unterpositionen mit Preisen für Einkauf
     
     re = requests.post(url=url,headers=headers,json=content)
 
@@ -40,3 +38,23 @@ def create_article(article_number, article_name):
         print(f"Fehler beim Erstellen von Artikel {article_number}")
         print("Status Code:", re.status_code, "Content:", re.content)
 
+
+# further infos for next steps:
+
+
+#   'salesBillOfMaterialItems': [{'id': '805694',
+#     'version': '0',
+#     'articleId': '82463',
+#     'articleNumber': 'G.NG',
+#     'createdDate': 1721119433142,
+#     'lastModifiedDate': 1721119433141,
+#     'positionNumber': 1,
+#     'quantity': '24'},
+#    {'id': '805695',
+#     'version': '0',
+#     'articleId': '111653',
+#     'articleNumber': 'G.NG_PB',
+#     'createdDate': 1721119433143,
+#     'lastModifiedDate': 1721119433142,
+#     'positionNumber': 2,
+#     'quantity': '6'},
